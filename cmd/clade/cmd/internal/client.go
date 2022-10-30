ee03332b2877905e673d3ee2fd34908aded1592a
Seunghyun Hwang <lesomnus@gmail.com>
2022-10-31 00:57:08 +0900

    get manifest from multi-arch image

--- cmd/clade/cmd/internal/client.go
+++ cmd/clade/cmd/internal/client.go
@@ -13,6 +13,7 @@ import (
 	"github.com/docker/distribution"
 	"github.com/docker/distribution/registry/api/errcode"
 	v2 "github.com/docker/distribution/registry/api/v2"
+	"github.com/opencontainers/go-digest"
 )
 
 type authTransport struct {
@@ -58,6 +59,49 @@ func NewTransport(ref reference.Named, actions ...string) http.RoundTripper {
 func NewRepository(ref reference.Named, actions ...string) (distribution.Repository, error) {
 	name_only, _ := reference.WithName(reference.Path(ref))
 	return client.NewRepository(name_only, "https://"+reference.Domain(ref), NewTransport(ref, actions...))
+}
+
+type ManifestGetter struct {
+	svc distribution.ManifestService
+	tag string
+	img digest.Digest // Digest for image
+}
+
+func NewManifestGetter(ctx context.Context, ref reference.NamedTagged) (*ManifestGetter, error) {
+	repo, err := NewRepository(ref)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create repository accessor: %w", err)
+	}
+
+	img_desc, err := repo.Tags(ctx).Get(ctx, ref.Tag())
+	if err != nil {
+		var errs errcode.Errors
+		if errors.As(err, &errs) {
+			for _, e := range errs {
+				if errors.Is(e, v2.ErrorCodeManifestUnknown) ||
+					errors.Is(e, errcode.ErrorCode(1003)) { // ghcr.io returns 1003 if manifest not exists
+					return nil, ErrManifestUnknown
+				}
+			}
+		}
+
+		return nil, fmt.Errorf("failed to get image descriptor: %w", err)
+	}
+
+	svc, err := repo.Manifests(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("failed to create manifest service: %w", err)
+	}
+
+	return &ManifestGetter{svc, ref.Tag(), img_desc.Digest}, nil
+}
+
+func (g *ManifestGetter) GetByDigest(ctx context.Context, digest digest.Digest) (distribution.Manifest, error) {
+	return g.svc.Get(ctx, digest)
+}
+
+func (g *ManifestGetter) Get(ctx context.Context) (distribution.Manifest, error) {
+	return g.svc.Get(ctx, g.img, distribution.WithTag(g.tag))
 }
 
 func GetManifest(ctx context.Context, ref reference.NamedTagged) (distribution.Manifest, error) {
