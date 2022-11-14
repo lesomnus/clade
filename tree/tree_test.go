37b1eaa71b75ebfafc2b73daef42b1fa57409ed7
Seunghyun Hwang <lesomnus@gmail.com>
2022-11-15 01:09:12 +0900

    add test

--- tree/tree_test.go
+++ tree/tree_test.go
@@ -16,19 +16,25 @@ func TestTree(t *testing.T) {
 	tr.Insert("mammalia", "human", 2)
 	mammalia := tr.Insert("animal", "mammalia", 1)
 
-	tr.Insert("amphibians", "frog", 2)
-	tr.Insert("amphibians", "salamander", 2)
-	amphibians := tr.Insert("animal", "amphibians", 1)
+	tr.Insert("amphibian", "frog", 2)
+	tr.Insert("amphibian", "salamander", 2)
+	amphibian := tr.Insert("animal", "amphibian", 1)
+
+	tr.Insert("fungi", "fungi", 0)
 
 	root := tr.AsNode()
-	require.Len(root.Children, 1)
+	require.Len(root.Children, 2)
 
 	animal, ok := root.Children["animal"]
 	require.True(ok)
 	require.Equal(animal, mammalia.Parent)
-	require.Equal(animal, amphibians.Parent)
+	require.Equal(animal, amphibian.Parent)
 	require.Contains(animal.Children, "mammalia")
-	require.Contains(animal.Children, "amphibians")
+	require.Contains(animal.Children, "amphibian")
+
+	fungi, ok := root.Children["fungi"]
+	require.True(ok)
+	require.Empty(fungi.Children)
 
 	// Note that root node is not visited.
 	root.Walk(func(level int, name string, node *tree.Node[int]) error {
